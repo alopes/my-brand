7adf946a35f26a459941b423a2d4d347a3c5fe7a
António Lopes <antonio@alopes.co.uk>
2012-10-02 10:49:18 +0100

    Startin up the projec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybrand::Application.config.secret_token = 'cf9296b27340270ec2e427e4e21f7ee2087e3cc8b95969618736930ff1e3b4020f04ef8b45cbd6f63b9fcd03f1e9eed28958bc7625e4a7c641d5d3e0290ff442'
